652c5ecdcd2aa8a69483bd3a20f24f30a887eb4f
Anass-Dr <anasdrissi77@gmail.com>
2024-10-01 21:39:18 +0100

    Fix: cors problem with front application server

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const apiRoutes = require("./routes/api");
 const requestValidator = require("./middleware/requestValidator");
 
-
 const app = express();
 
+const corsOptions = {
+  origin: process.env.FRONT_APP_HOST,
+  optionsSuccessStatus: 200,
+};
+
 // middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("public"));
